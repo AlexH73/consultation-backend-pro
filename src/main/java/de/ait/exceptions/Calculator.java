045994386eaf518396b1ca2499ff9df51aa6baf0
Alexander Hermann <ewebotah@gmail.com>
2026-01-11 20:59:13 +0100

    feat(exceptions): add custom exception and handling examples for division and file I/O

---
+++ src/main/java/de/ait/exceptions/Calculator.java
@@ -0,0 +1,12 @@
+package de.ait.exceptions;
+
+public class Calculator {
+    public static void main(String[] args) {
+        DivisionExample divisionExample = new DivisionExample();
+        try {
+            divisionExample.divide(10, -2);
+        } catch (NegativeNumberException exception) {
+            System.out.println("Negative numbers are not allowed!");
+        }
+    }
+}
